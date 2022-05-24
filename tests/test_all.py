a06a5c53d7c3df282ec398a6ac2ead142bf11507
jpcurrea <johnpaulcurrea@gmail.com>
2022-05-24 15:07:28 -0700

    Clean up tests folder and add test_all.py for running all 3 tests

---
+++ tests/test_all.py
@@ -0,0 +1,8 @@
+print("Running 3 tests:")
+print("1. Test the ODA on a single image:")
+import test_layer
+print("2. Test the ODA on a stack of microscope images:")
+import test_stack
+print("3. Test the ODA on a stack of microscope images:")
+import test_CT
+test_files = ['test_layer.py', 'test_stack.py', 'test_CT.py']
