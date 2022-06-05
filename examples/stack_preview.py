9e429b0697397426c2224891914fde3ae2fb98d0
jpcurrea <johnpaulcurrea@gmail.com>
2022-06-05 11:19:55 -0700

    Add 3D preview to console-scripts in setup.cfg for pip install.

---
+++ examples/stack_preview.py
@@ -0,0 +1,18 @@
+"""A command line tool to preview ODA results in 3D."""
+from ODA import CTStack
+import sys
+
+PIXEL_SIZE = .325               # mm
+BIN_SIZE = 4                    # voxel bin size
+PIXEL_SIZE *= BIN_SIZE          # adjust pixel size by bin size
+
+
+if __name__=="__main__":
+    # check for user specified directory name
+    # use current directory by default
+    folder = "./"
+    if len(sys.argv) > 1:
+        folder = sys.argv[1]
+    stack = CTStack(dirname=folder, img_extension='.tif', bw=True,
+                    pixel_size=PIXEL_SIZE, depth_size=PIXEL_SIZE)
+    stack.plot_data_3d()
